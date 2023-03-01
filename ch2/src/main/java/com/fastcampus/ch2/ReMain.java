56f2b721c1360ba85192c257ea9bf8a5a5ef1d34
HoSeung <meteorproject529@gamil.com>
2023-03-02 02:35:44 +0900

    git 연동 시작

---
+++ ch2/src/main/java/com/fastcampus/ch2/ReMain.java
@@ -0,0 +1,26 @@
+package com.fastcampus.ch2;
+
+import java.lang.reflect.Method;
+
+public class ReMain {
+
+	public static void main(String[] args) throws Exception{
+
+//		Hello hello = new Hello();
+//		hello.main();
+		
+//		Reflection API를 사용 - 클래스 정보를 얻고 다룰 수 있는 강력한 기능 제공
+//		java.lang.reflect 패키지를 제공
+		// Hello클래스의 Class객체(클래스의 정보를 담고 있는 객체)를 얻어온다.
+		
+//		Class helloClass = Class.forName("com.fastcampus.ch2.Hello");
+//		Hello hello = (Hello) helloClass.newInstance();
+//		Method main = helloClass.getDeclaredMethod("main");
+//		main.setAccessible(true); 
+//		
+//		main.invoke(hello);
+		
+		
+	}
+
+}
